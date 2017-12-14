0b984e04ad430df0b0de559b9e897079d7c6d977
davidliu <david.liu@oneplus.net>
2017-12-14 14:07:17 +0800

    Synchronize codes for OnePlus5 O OPEN BETA 2 kernel device tree source code for OnePlus 5 O device
    Change-Id: Id0bc549c21988b933f174a43d5da15167784beb9

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4254,11 +4254,6 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 		return;
 #endif
 
-	if ((udev->bos->ext_cap->bmAttributes & cpu_to_le32(USB_BESL_SUPPORT)) ||
-			connect_type == USB_PORT_CONNECT_TYPE_HARD_WIRED) {
-		udev->usb2_hw_lpm_allowed = 1;
-		usb_set_usb2_hardware_lpm(udev, 1);
-	}
 }
 
 static int hub_enable_device(struct usb_device *udev)
