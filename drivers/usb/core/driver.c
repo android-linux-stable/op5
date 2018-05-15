LineageOS/android_kernel_oneplus_msm8998

drivers: misc OnePlus source imports
Change-Id: I7a821fdd2a8a1eb5f5db8e10724757b2796b9efb
Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

drivers/usb/core/driver.c
@@ -1785,6 +1785,14 @@ static int autosuspend_check(struct usb_device *udev)
 			 * or else their drivers don't support autosuspend
 			 * and so they are permanently active.
 			 */
+
+#ifdef VENDOR_CONFIG_ONEPLUS
+			if (intf == NULL) {
+				dev_err(&udev->dev, "%s intf = NULL\n", __func__);
+				return -EIO;
+			}
+#endif
+
 			if (intf->dev.power.disable_depth)
 				continue;
 			if (atomic_read(&intf->dev.power.usage_count) > 0)

android-linux-stable/op5

Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

drivers/usb/core/driver.c
@@ -1806,6 +1806,14 @@ static int autosuspend_check(struct usb_device *udev)
 			 * or else their drivers don't support autosuspend
 			 * and so they are permanently active.
 			 */
+
+#ifdef CONFIG_VENDOR_ONEPLUS
+			if (intf == NULL) {
+				dev_err(&udev->dev, "%s intf = NULL\n", __func__);
+				return -EIO;
+			}
+#endif
+
 			if (intf->dev.power.disable_depth)
 				continue;
 			if (atomic_read(&intf->dev.power.usage_count) > 0)